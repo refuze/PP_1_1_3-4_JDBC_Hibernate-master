ce69da7e07ae780feddd87fc8bd753e2f013c835
refuze <refuzeapple@gmail.com>
2022-11-05 12:25:24 +0300

    First hotfix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,7 +8,7 @@ public class Util {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/kataschema";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1234";
-    private static Connection connection;
+    private static final Connection connection;
 
     static {
         try {
